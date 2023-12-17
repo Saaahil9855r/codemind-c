5fe3e31bb58b5603d65bd31c8e76ee0df39ed9af
Sahil Raj <134907591+Saaahil9855r@users.noreply.github.com>
2023-12-18 01:09:22 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int c=0;     for(int i=1;i<a;i++){         if(a%i==0) {             c=c+i;         }     }     if(c==a){         printf("PERFECT");     }     else if(c<a){         printf("DEFICIENT");     }     else {         printf("ABUNDANT");     } }

---
+++ PDA.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int c=0;
+    for(int i=1;i<a;i++){
+        if(a%i==0) {
+            c=c+i;
+        }
+    }
+    if(c==a){
+        printf("PERFECT");
+    }
+    else if(c<a){
+        printf("DEFICIENT");
+    }
+    else {
+        printf("ABUNDANT");
+    }
+}
