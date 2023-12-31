3531c2318650718255e43bb419e832f276ff6c98
Sahil Raj <134907591+Saaahil9855r@users.noreply.github.com>
2023-12-31 09:50:01 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int b=a;     for(int i=1;i<=a;i++){         for(int j=1;j<=a+1-i;j++){             printf("%d",b);         }         b--;         for(int k=1;k<i;k++){             printf("*");         }         printf(" ");     } }

---
+++ Basic_Pattern_5.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b=a;
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=a+1-i;j++){
+            printf("%d",b);
+        }
+        b--;
+        for(int k=1;k<i;k++){
+            printf("*");
+        }
+        printf("
+");
+    }
+}
